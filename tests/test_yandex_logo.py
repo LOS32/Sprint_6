e86b2a6d2768eb13e91552f1cb824ea0d0bd9862
Dmitrii Losunov <dmitrij.dmitriy32@yandex.ru>
2024-11-23 22:55:44 -0800

    Добавлены тесты для логотипа Яндекса, исправлены файлы main_page.py

---
+++ tests/test_yandex_logo.py
@@ -0,0 +1,20 @@
+import allure
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pages.main_page import MainPage
+from data_questions import URLs
+from conftest import driver
+
+
+@allure.title('Тест на проверку логотипа Яндекса')
+class TestYandexLogo:
+    def test_yandex_logo_redirect(self, driver):
+        main_page = MainPage(driver)
+        main_page.open_main_page()
+        main_page.click_to_cookie_button()
+        main_page.click_yandex_logo()
+        WebDriverWait(driver, 10).until(lambda d: len(d.window_handles) > 1)
+        driver.switch_to.window(driver.window_handles[1])
+        WebDriverWait(driver, 10).until(EC.url_contains(URLs.DZEN_URL))
+        driver.switch_to.window(driver.window_handles[1])
+        assert driver.current_url == URLs.DZEN_URL
