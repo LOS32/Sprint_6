3528c0da847af44109505b2ec8a6f154cd5ed231
Dmitrii Losunov <dmitrij.dmitriy32@yandex.ru>
2024-11-23 22:38:44 -0800

    Написан тест на проверку логотипа самокат

---
+++ tests/test_scooter_logo.py
@@ -0,0 +1,16 @@
+import allure
+from locators.main_page_locators import MainPageLocators
+from pages.main_page import MainPage
+from data_questions import URLs
+from conftest import driver
+
+
+@allure.title('Тест на проверку логотипа Самоката')
+class TestScooterLogo:
+    def test_scooter_logo_redirect(self, driver):
+        main_page = MainPage(driver)
+        main_page.open_main_page()
+        main_page.click_to_cookie_button()
+        main_page.click_to_order_button(MainPageLocators.ORDER_BUTTON_HEADER)
+        main_page.click_scooter_logo()
+        assert driver.current_url == URLs.BASE_URL
